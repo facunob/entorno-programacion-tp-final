ee9e06dc136cd5bd72f968b16ef977faed42e546
Facundo Fontela <facufonte@hotmail.com>
2022-07-03 15:59:37 -0300

    primera version script 1

---
+++ scripts/statsWords.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ROUTE_FILE=$1
+#Validar algo?
+
+
+NUMBER_OF_WORDS=0
+LENGTH_COUNTER=0
+LONGEST_WORD=""
+SHORTEST_WORD=""
+for x in $(cat $ROUTE_FILE)
+do
+	LENGTH=${#x}
+	NUMBER_OF_WORDS=$(($NUMBER_OF_WORDS+1))
+	LENGTH_COUNTER=$(($LENGTH_COUNTER+$LENGTH))
+	[[ $(($LENGTH)) -gt $((${#LONGEST_WORD})) ]] && LONGEST_WORD=$x
+	[[ $(($LENGTH)) -lt $((${#SHORTEST_WORD})) ]] || [[ $((${#SHORTEST_WORD})) == 0 ]] && SHORTEST_WORD=$x
+done
+
+echo "Numero de palabras en el texto: $NUMBER_OF_WORDS"
+PROM=$(($LENGTH_COUNTER / $NUMBER_OF_WORDS))
+echo "Promedio de longitud de las palabras: $PROM"
+echo "La palabra mas larga es: $LONGEST_WORD"
+echo "La palabra mas corta es: $SHORTEST_WORD"
